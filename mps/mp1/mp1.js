9015b60356e67ee0919426a0b8bc72a4982f8688
Nikhil Simha <simha3@illinois.edu>
2019-09-19 04:18:43 -0500

    daily push

--- mps/mp1/mp1.js
+++ mps/mp1/mp1.js
@@ -21,8 +21,8 @@ var vertexColorBuffer;
 /** @global The angle of rotation around the x axis */
 var defAngle = 0;
 
-// /** @global The amount of translation in the x direction*/
-// var transRights = [0, 0, 0];
+/** @global The amount of translation in the x direction*/
+var transRights = [0, 0, 0];
 
 /** @global The amount the model should be scaled by */
 var scalefac = 1;
@@ -43,6 +43,14 @@ var anim_value;
 var anim_value_prev = "Illini";
 
 var mode = 0;
+
+var colfac = 1;
+var colsign = 1;
+var colorlad1 = [0, 0, 0, 1.0];
+var colorlad2 = [0, 0, 0, 1.0];
+var colorlad3 = [0, 0, 0, 1.0];
+var colorlad4 = [0, 0, 0, 1.0];
+var colorlad5 = [0, 0, 0, 1.0];
 
 
 /**
@@ -237,7 +245,7 @@ function animate() {
   if (scalefac <= 1) {
     sign *= -1;
   }
-
+  console.log(scalefac);
   var tmpMatrix = mat4.create();
   var tmp2Matrix = mat4.create();
   mat4.fromZRotation(tmpMatrix, degToRad(defAngle));
@@ -664,22 +672,52 @@ function tick() {
 
 
 function animate_shit() { 
-  defAngle = (defAngle+1.0) % 360;
-
-  scalefac += (sign * 0.01);
-  if (scalefac >= 2) {
-    sign *= -1;
+  colfac += (colsign * 0.01);
+  if (colfac >= 2) {
+    colsign *= -1;
   }
-  if (scalefac <= 1) {
-    sign *= -1;
+  if (colfac <= 1) {
+    colsign *= -1;
+  }
+  console.log(colfac);
+  defAngle = (defAngle+1.0) % 1000;
+
+  if (defAngle % 250 == 0) {
+    colorlad1 = [Math.random(), Math.random(), Math.random(), colfac-1];
+  }
+  if (defAngle % 500 == 6) {
+    colorlad2 = [Math.random(), Math.random(), Math.random(), colfac-1];
+  }
+  if (defAngle % 500 == 9) {
+    colorlad3 = [Math.random(), Math.random(), Math.random(), colfac-1];
+  }
+  if (defAngle % 500 == 11) {
+    colorlad4 = [Math.random(), Math.random(), Math.random(), colfac-1];
+  }
+  if (defAngle % 500 == 13) {
+    colorlad5 = [Math.random(), Math.random(), Math.random(), colfac-1];
   }
 
-  var tmpMatrix = mat4.create();
-  var tmp2Matrix = mat4.create();
-  mat4.fromZRotation(tmpMatrix, degToRad(defAngle));
-  mat4.scale(tmp2Matrix, tmp2Matrix, [scalefac, scalefac, scalefac]);
+  if (defAngle % 4 == 0 && (colfac - 1) < 0.9){
+    colorlad1[3] = colfac - 1;
+    colorlad2[3] = colfac - 1;
+    colorlad3[3] = colfac - 1;
+    colorlad4[3] = colfac - 1;
+    colorlad5[3] = colfac - 1;
+  }
+
   
-  mat4.multiply(mvMatrix, tmp2Matrix, tmpMatrix);
+
+  // transRights = [10, 0, 0];
+
+  // var tmpMatrix = mat4.create();
+  // var tmp2Matrix = mat4.create();
+  // mvMatrix = mat4.create();
+  mat4.fromZRotation(mvMatrix, degToRad(0));  
+  // mat4.scale(tmp2Matrix, tmp2Matrix, [scalefac, scalefac, scalefac]);
+  // mat4.fromTranslation(mvMatrix, transRights);
+  
+  // mat4.multiply(mvMatrix, tmp2Matrix, tmpMatrix);
 
   gl.uniformMatrix4fv(shaderProgram.uMVMatrixUniform, false, mvMatrix);
 
@@ -690,342 +728,110 @@ function animate_shit() {
 
   //change color to orange and setup buffer for orange I
   // 0.909, 0.290, 0.152, 1.0
-  gl.uniform4fv( vColor, new Float32Array([0.909, 0.290, 0.152, 1.0]) );
-  
+  gl.uniform4fv( vColor, new Float32Array(colorlad1) );
+
   triVertices = [
-    -20,  35,  0.0, // orange I
-      20,  35,  0.0,
-      20,  15,  0.0,
-    -20,  35,  0.0,
-    -20,  15,  0.0,
-      20,  15,  0.0,
-    -20, -35,  0.0,
-      20, -35,  0.0,
-      20, -15,  0.0,
-    -20, -15,  0.0,
-    -20, -35,  0.0,
-      20, -15,  0.0,
-    -10,  15,  0.0,
-    -10, -15,  0.0,
-      10, -15,  0.0,
-      10,  15,  0.0,
-      10, -15,  0.0,
-    -10,  15,  0.0,
+    -50,  50,  0.0, // stick figure head and body
+      -50,  -50,  0.0,
+      50,  50,  0.0,
+    -50,  -50,  0.0,
+    50,  -50,  0.0,
+      50,  50,  0.0,
   ];
 
-  // for (var i = 0; i < triVertices.length; i++) {
-  //   if (i % 3 == 0) {
-  //     defPt = deformCos(triVertices[i], triVertices[i+1], defAngle);
-  //     triVertices[i] += defPt[0];
-  //     triVertices[i+1] += defPt[1];
-  //   }
-  // }
-  setupBuffers(triVertices, 3, 18); //provide arguments with colors and coordinates, as well as num of items and shits
+  setupBuffers(triVertices, 3, 6); //provide arguments with colors and coordinates, as well as num of items and shits
   draw(0);  
 
   //change color to blue and setup buffer for blue border!
   // 0.074, 0.160, 0.294, 1.0
-  gl.uniform4fv( vColor, new Float32Array([0.074, 0.160, 0.294, 1.0]) );
+  gl.uniform4fv( vColor, new Float32Array(colorlad2) );
   triVertices = [
-    -16,  15,  0.0, // top arc 18
-    -2,  15,  0.0,
-    -2,  35,  0.0, // 7
-    -16,  15,  0.0,
-    -2,  35,  0.0, // 13
-    -16,  35,  0.0, // 16
-    -16,  35,  0.0, // 19
-    -16,  31,  0.0,
-      16,  35,  0.0, // 25
-    -16,  31,  0.0,
-      16,  35,  0.0,  // 31
-      16,  31,  0.0,
-      16,  35,  0.0, // 37
-      2,  35,  0.0, // 40
-      2,  15,  0.0,
-      2,  15,  0.0,
-      16,  35,  0.0, // 49
-      16,  15,  0.0,
-    -16, -15,  0.0, // bottom arc 18
-    -2, -15,  0.0,
-    -2, -35,  0.0,
-    -16, -15,  0.0,
-    -2, -35,  0.0,
-    -16, -35,  0.0,
-    -16, -35,  0.0,
-    -16, -31,  0.0,
-      16, -35,  0.0,
-    -16, -31,  0.0,
-      16, -35,  0.0, 
-      16, -31,  0.0,
-      16, -35,  0.0,
-      2, -35,  0.0,
-      2, -15,  0.0,
-      2, -15,  0.0,
-      16, -35,  0.0,
-      16, -15,  0.0,
-    -16,  15,  0.0, // left liney bois 12
-    -16,  19,  0.0,
-    -10,  19,  0.0,
-    -10,  19,  0.0,
-    -10,  15,  0.0,
-    -16,  15,  0.0,
-    -16, -15,  0.0,
-    -16, -19,  0.0,
-    -10, -19,  0.0,
-    -10, -19,  0.0,
-    -10, -15,  0.0,
-    -16, -15,  0.0,
-    16,  15,  0.0,  // right liney bois 12
-    16,  19,  0.0,
-    10,  19,  0.0,
-    10,  19,  0.0,
-    10,  15,  0.0,
-    16,  15,  0.0,
-    16, -15,  0.0,
-    16, -19,  0.0,
-    10, -19,  0.0,
-    10, -19,  0.0,
-    10, -15,  0.0,
-    16, -15,  0.0,
-    -10,  15,  0.0,    // vertical line bois 12
-      -6,  15,  0.0,
-    -10, -15,  0.0,
-    -6, -15,  0.0,
-    -10, -15,  0.0,
-      -6,  15,  0.0,
-      10,  15,  0.0,
-      6,  15,  0.0,
-    10, -15,  0.0,
-    6, -15,  0.0,
-    10, -15,  0.0,
-      6,  15,  0.0, // this marks the 72nd, 0.0 is 215
-     
-    -8,  16,  0.0,    // 1st popout lad
-    -8,  14,  0.0,
-    -10, 15,  0.0,
-    -8, 14,  0.0, // 2nd popout lad
-    -8, 12,  0.0,
-    -10, 13,  0.0,
-    -8,  12,  0.0, // 3rd popout lad
-    -8, 10,  0.0,
-    -10, 11,  0.0,
-    -8, 10,  0.0, // 4th popout lad
-    -8, 8,  0.0,
-    -10, 9,  0.0,
-    -8, 8,  0.0, // 5th popout lad
-    -8, 6,  0.0,
-    -10, 7,  0.0,
-    -8, 6,  0.0, // 6th popout lad
-    -8, 4,  0.0,
-    -10, 5,  0.0,
-    -8, 4,  0.0, // 7th popout lad
-    -8, 2,  0.0,
-    -10, 3,  0.0,
-    -8, 2,  0.0, // 8th popout lad
-    -8, 0,  0.0,
-    -10, 1,  0.0,
-    -8, 0,  0.0, // 9th popout lad
-    -8, -2,  0.0,
-    -10, -1,  0.0,
-    -8, -2,  0.0, // 10th popout lad
-    -8, -4,  0.0,
-    -10, -3,  0.0,
-    -8, -4,  0.0, // 11th popout lad
-    -8, -6,  0.0,
-    -10, -5,  0.0,
-    -8, -6,  0.0, // 12th popout lad
-    -8, -8,  0.0,
-    -10, -7,  0.0,
-    -8, -8,  0.0, // 13th popout lad
-    -8, -10,  0.0,
-    -10, -9,  0.0,
-    -8, -10,  0.0, // 14th popout lad
-    -8, -12,  0.0,
-    -10, -11,  0.0,
-    -8, -12,  0.0, // 15th popout lad
-    -8, -14,  0.0,
-    -10, -13,  0.0,
-    -8, -14,  0.0, // 16th popout lad
-    -8, -16,  0.0,
-    -10, -15,  0.0,
-
-    8,  16,  0.0,    // 1st popout lad
-    8,  14,  0.0,
-    10, 15,  0.0,
-    8, 14,  0.0, // 2nd popout lad
-    8, 12,  0.0,
-    10, 13,  0.0,
-    8,  12,  0.0, // 3rd popout lad
-    8, 10,  0.0,
-    10, 11,  0.0,
-    8, 10,  0.0, // 4th popout lad
-    8, 8,  0.0,
-    10, 9,  0.0,
-    8, 8,  0.0, // 5th popout lad
-    8, 6,  0.0,
-    10, 7,  0.0,
-    8, 6,  0.0, // 6th popout lad
-    8, 4,  0.0,
-    10, 5,  0.0,
-    8, 4,  0.0, // 7th popout lad
-    8, 2,  0.0,
-    10, 3,  0.0,
-    8, 2,  0.0, // 8th popout lad
-    8, 0,  0.0,
-    10, 1,  0.0,
-    8, 0,  0.0, // 9th popout lad
-    8, -2,  0.0,
-    10, -1,  0.0,
-    8, -2,  0.0, // 10th popout lad
-    8, -4,  0.0,
-    10, -3,  0.0,
-    8, -4,  0.0, // 11th popout lad
-    8, -6,  0.0,
-    10, -5,  0.0,
-    8, -6,  0.0, // 12th popout lad
-    8, -8,  0.0,
-    10, -7,  0.0,
-    8, -8,  0.0, // 13th popout lad
-    8, -10,  0.0,
-    10, -9,  0.0,
-    8, -10,  0.0, // 14th popout lad
-    8, -12,  0.0,
-    10, -11,  0.0,
-    8, -12,  0.0, // 15th popout lad
-    8, -14,  0.0,
-    10, -13,  0.0,
-    8, -14,  0.0, // 16th popout lad
-    8, -16,  0.0,
-    10, -15,  0.0,
+    -50,  50,  0.0, // top arc 18
+    -100,  100,  0.0,
+    100,  100,  0.0, // 7
+    100,  100,  0.0,
+    50,  50,  0.0, // 13
+    -50,  50,  0.0, // 16
   ];
 
-  // vertexAnimIndices = [7, 13, 16, 19, 25, 31, 37, 40, 49];
-  // non-uniform transform lol
-  if (defAngle < 45) {
-    triVertices[222] *= 1.5;
-    triVertices[294] *= 1.5;
-    triVertices[402] *= 1.5;
-    triVertices[474] *= 1.5;
-  }
-  else if (defAngle >= 45 && defAngle < 90) {
-    triVertices[231] *= 1.8;
-    triVertices[303] *= 1.8;
-    triVertices[411] *= 1.8;
-    triVertices[483] *= 1.8;
-  }
-  else if (defAngle >= 90 && defAngle < 135) {
-    triVertices[240] *= 2;
-    triVertices[312] *= 2;
-    triVertices[42] *= 2; // blaze it
-    triVertices[492] *= 2;
-  }
-  else if (defAngle >= 135 && defAngle < 180) {
-    triVertices[249] *= 1.8;
-    triVertices[321] *= 1.8;
-    triVertices[429] *= 1.8;
-    triVertices[501] *= 1.8;
-  }
-  else if (defAngle >= 180 && defAngle < 225) {
-    triVertices[258] *= 1.5;
-    triVertices[330] *= 1.5;
-    triVertices[366] *= 1.5;
-    triVertices[438] *= 1.5;
-  }
-  else if (defAngle >= 225 && defAngle < 270) {
-    triVertices[267] *= 1.8;
-    triVertices[339] *= 1.8;
-    triVertices[375] *= 1.8;
-    triVertices[447] *= 1.8;
-  }
-  else if (defAngle >= 270 && defAngle < 315) {
-    triVertices[276] *= 2;
-    triVertices[348] *= 2;
-    triVertices[384] *= 2;
-    triVertices[456] *= 2;
-  }
-  else if (defAngle >= 315 && defAngle < 360) {
-    triVertices[285] *= 1.8;
-    triVertices[357] *= 1.8;
-    triVertices[393] *= 1.8;
-    triVertices[465] *= 1.8;
-  }
-  
-  // for (var i = 0; i < triVertices.length; i++) {
-  //   if (i % 3 == 0) {
-  //     defPt = deformCos(triVertices[i], triVertices[i+1], defAngle);
-  //     triVertices[i] += defPt[0];
-  //     triVertices[i+1] += defPt[1];
-  //   }
-  // }
-
-  setupBuffers(triVertices, 3, 168);
+  setupBuffers(triVertices, 3, 6);
   draw(0);
 
-  var radius = 4;
-  var z = 0;
-  var defPt;
+  gl.uniform4fv( vColor, new Float32Array(colorlad3) );
+  triVertices = [
+    -50,  50,  0.0, // top arc 18
+    -100,  100,  0.0,
+    -100,  -100,  0.0, // 7
+    -100,  -100,  0.0,
+    -50,  -50,  0.0, // 13
+    -50,  50,  0.0, // 16
+  ];
 
-  fanVertices = [-10, 15, 0];
-  for (i = 0; i <= numCircleVerts; i++) {
-    angle = i * (Math.PI/2) / numCircleVerts;
-    x = (radius * Math.cos(angle));
-    y = (radius * Math.sin(angle));
-    // defPt = deformCos(x, y, angle);
-    // fanVertices.push(x-10+defPt[0]);
-    // fanVertices.push(y+15+defPt[1]);
-    fanVertices.push(x-10);
-    fanVertices.push(y+15);
-    fanVertices.push(z);
-  }
+  setupBuffers(triVertices, 3, 6);
+  draw(0);
 
-  setupBuffers(fanVertices, 3, numCircleVerts+2);
-  draw(1);
+  gl.uniform4fv( vColor, new Float32Array(colorlad4) );
+  triVertices = [
+    50,  50,  0.0, // top arc 18
+    100,  100,  0.0,
+    100,  -100,  0.0, // 7
+    100,  -100,  0.0,
+    50,  -50,  0.0, // 13
+    50,  50,  0.0, // 16
+  ];
 
-  fanVertices = [10, 15, 0];
-  for (i = 0; i <= numCircleVerts; i++) {
-    angle = (Math.PI/2) + i * (Math.PI/2) / numCircleVerts;
-    x = (radius * Math.cos(angle));
-    y = (radius * Math.sin(angle));
-    // defPt = deformCos(x, y, angle);
-    // fanVertices.push(x+10+defPt[0]);
-    // fanVertices.push(y+15+defPt[1]);
-    fanVertices.push(x+10);
-    fanVertices.push(y+15);
-    fanVertices.push(z);
-  }
+  setupBuffers(triVertices, 3, 6);
+  draw(0);
 
-  setupBuffers(fanVertices, 3, numCircleVerts+2);
-  draw(1);
+  gl.uniform4fv( vColor, new Float32Array(colorlad5) );
+  triVertices = [
+    -50,  -50,  0.0, // top arc 18
+    -100,  -100,  0.0,
+    50,  -50,  0.0, // 7
+    -100,  -100,  0.0,
+    50,  -50,  0.0, // 13
+    100,  -100,  0.0, // 16
+  ];
 
-  fanVertices = [-10, -15, 0];
-  for (i = 0; i <= numCircleVerts; i++) {
-    angle = (3*Math.PI/2) + i * (Math.PI/2) / numCircleVerts;
-    x = (radius * Math.cos(angle));
-    y = (radius * Math.sin(angle));
-    // defPt = deformCos(x, y, angle);
-    // fanVertices.push(x-10+defPt[0]);
-    // fanVertices.push(y-15+defPt[1]);
-    fanVertices.push(x-10);
-    fanVertices.push(y-15);
-    fanVertices.push(z);
-  }
+  setupBuffers(triVertices, 3, 6);
+  draw(0);
 
-  setupBuffers(fanVertices, 3, numCircleVerts+2);
-  draw(1);
+  // fanVertices = [10, 15, 0];
+  // for (i = 0; i <= numCircleVerts; i++) {
+  //   angle = (Math.PI/2) + i * (Math.PI/2) / numCircleVerts;
+  //   x = (radius * Math.cos(angle));
+  //   y = (radius * Math.sin(angle));
+  //   fanVertices.push(x+10);
+  //   fanVertices.push(y+15);
+  //   fanVertices.push(z);
+  // }
 
-  fanVertices = [10, -15, 0];
-  for (i = 0; i <= numCircleVerts; i++) {
-    angle = (Math.PI) + i * (Math.PI/2) / numCircleVerts;
-    x = (radius * Math.cos(angle));
-    y = (radius * Math.sin(angle));
-    // defPt = deformCos(x, y, angle);
-    // fanVertices.push(x+10+defPt[0]);
-    // fanVertices.push(y-15+defPt[1]);
-    fanVertices.push(x+10);
-    fanVertices.push(y-15);
-    fanVertices.push(z);
-  }
+  // setupBuffers(fanVertices, 3, numCircleVerts+2);
+  // draw(1);
 
-  setupBuffers(fanVertices, 3, numCircleVerts+2);
-  draw(1);
+  // fanVertices = [-10, -15, 0];
+  // for (i = 0; i <= numCircleVerts; i++) {
+  //   angle = (3*Math.PI/2) + i * (Math.PI/2) / numCircleVerts;
+  //   x = (radius * Math.cos(angle));
+  //   y = (radius * Math.sin(angle));
+  //   fanVertices.push(x-10);
+  //   fanVertices.push(y-15);
+  //   fanVertices.push(z);
+  // }
+
+  // setupBuffers(fanVertices, 3, numCircleVerts+2);
+  // draw(1);
+
+  // fanVertices = [10, -15, 0];
+  // for (i = 0; i <= numCircleVerts; i++) {
+  //   angle = (Math.PI) + i * (Math.PI/2) / numCircleVerts;
+  //   x = (radius * Math.cos(angle));
+  //   y = (radius * Math.sin(angle));
+  //   fanVertices.push(x+10);
+  //   fanVertices.push(y-15);
+  //   fanVertices.push(z);
+  // }
+
+  // setupBuffers(fanVertices, 3, numCircleVerts+2);
+  // draw(1);
 }
