7db74c6ecadc82770246fcbef02f7e8adfc139af
Nikhil Simha <simha3@illinois.edu>
2019-11-17 17:10:09 -0500

    skybox done, need to load in obj now

--- mps/mp3 temp/mp3.js
+++ mps/mp3 temp/mp3.js
@@ -25,6 +25,179 @@ var quadBufferInfo;
 /** @global the texture used for the cubemap */
 var texture;
 
+/** @global model parameters */
+var eulerY=0;
+
+/** @global origin */
+var target;
+
+/** @global camera's position, aka eyePt */
+var cameraPosition;
+
+/** @global A simple GLSL shader program */
+var shaderProgram;
+
+/** @global The Modelview matrix */
+var mvMatrix = mat4.create();
+
+/** @global The View matrix */
+var vMatrix = mat4.create();
+
+/** @global The Projection matrix */
+var pMatrix = mat4.create();
+
+/** @global The Normal matrix */
+var nMatrix = mat3.create();
+
+/** @global The matrix stack for hierarchical modeling */
+var mvMatrixStack = [];
+
+/** @global An object holding the geometry for a 3D mesh */
+var myMesh;
+
+// View parameters
+/** @global Location of the camera in world coordinates */
+var eyePt = vec3.fromValues(0.0,0.0,2.0);
+/** @global Direction of the view in world coordinates */
+var viewDir = vec3.fromValues(0.0,0.0,-1.0);
+/** @global Up vector for view matrix creation, in world coordinates */
+var up = vec3.fromValues(0.0,1.0,0.0);
+/** @global Location of a point along viewDir in world coordinates */
+var viewPt = vec3.fromValues(0.0,0.0,0.0);
+
+//Light parameters
+/** @global Light position in VIEW coordinates */
+var lightPosition = [0,5,5];
+/** @global Ambient light color/intensity for Phong reflection */
+var lAmbient = [0,0,0];
+/** @global Diffuse light color/intensity for Phong reflection */
+var lDiffuse = [1,1,1];
+/** @global Specular light color/intensity for Phong reflection */
+var lSpecular =[0,0,0];
+
+//Material parameters
+/** @global Ambient material color/intensity for Phong reflection */
+var kAmbient = [1.0,1.0,1.0];
+/** @global Diffuse material color/intensity for Phong reflection */
+var kTerrainDiffuse = [205.0/255.0,163.0/255.0,63.0/255.0];
+/** @global Specular material color/intensity for Phong reflection */
+var kSpecular = [0.0,0.0,0.0];
+/** @global Shininess exponent for Phong reflection */
+var shininess = 23;
+/** @global Edge color fpr wireframeish rendering */
+var kEdgeBlack = [0.0,0.0,0.0];
+/** @global Edge color for wireframe rendering */
+var kEdgeWhite = [1.0,1.0,1.0];
+
+//-------------------------------------------------------------------------
+/**
+ * Asynchronously read a server-side text file
+ */
+function asyncGetFile(url) {
+  //Your code here
+  console.log("Getting text file");
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = () => resolve(xhr.responseText);
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+    console.log("Made promise");
+  });
+}
+
+//----------------------------------------------------------------------------------
+//Code to handle user interaction
+var currentlyPressedKeys = {};
+
+function handleKeyDown(event) {
+    //console.log("Key down ", event.key, " code ", event.code);
+    currentlyPressedKeys[event.key] = true;
+        if (currentlyPressedKeys["a"]) {
+        // key A
+        cameraPosition[0]-= 0.01;
+    } else if (currentlyPressedKeys["d"]) {
+        // key D
+        cameraPosition[0]+= 0.01;
+    } 
+
+    if (currentlyPressedKeys["ArrowLeft"]){ // TODO
+        // Left cursor key
+        event.preventDefault();
+        vec3.rotateY(cameraPosition, cameraPosition, target, 0.01);
+    } else if (currentlyPressedKeys["ArrowRight"]){
+        event.preventDefault();
+        // Right cursor key
+        vec3.rotateY(cameraPosition, cameraPosition, target, -0.01);
+    } 
+}
+
+function handleKeyUp(event) {
+    //console.log("Key up ", event.key, " code ", event.code);
+    currentlyPressedKeys[event.key] = false;
+}
+
+//-------------------------------------------------------------------------
+/**
+ * Sends Modelview matrix to shader
+ */
+function uploadModelViewMatrixToShader() {
+    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+}
+  
+//-------------------------------------------------------------------------
+/**
+ * Sends projection matrix to shader
+ */
+function uploadProjectionMatrixToShader() {
+    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, 
+                        false, pMatrix);
+}
+  
+//-------------------------------------------------------------------------
+/**
+ * Generates and sends the normal matrix to the shader
+*/
+function uploadNormalMatrixToShader() {
+    mat3.fromMat4(nMatrix,mvMatrix);
+    mat3.transpose(nMatrix,nMatrix);
+    mat3.invert(nMatrix,nMatrix);
+    gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, nMatrix);
+}
+  
+//----------------------------------------------------------------------------------
+/**
+ * Pushes matrix onto modelview matrix stack
+*/
+  function mvPushMatrix() {
+      var copy = mat4.clone(mvMatrix);
+      mvMatrixStack.push(copy);
+  }
+  
+  
+//----------------------------------------------------------------------------------
+/**
+ * Pops matrix off of modelview matrix stack
+ */
+function mvPopMatrix() {
+    if (mvMatrixStack.length == 0) {
+    throw "Invalid popMatrix!";
+    }
+    mvMatrix = mvMatrixStack.pop();
+}
+  
+//----------------------------------------------------------------------------------
+/**
+ * Sends projection/modelview matrices to shader
+ */
+function setMatrixUniforms() {
+    uploadModelViewMatrixToShader();
+    uploadNormalMatrixToShader();
+    uploadProjectionMatrixToShader();
+}
+
+
+
 //----------------------------------------------------------------------------------
 /**
  * Translates radians to degrees
@@ -45,6 +218,26 @@ function degToRad(degrees) {
     return degrees * Math.PI / 180;
 }
 
+//----------------------------------------------------------------------------------
+/**
+ * Populate buffers with data
+ */
+function setupMesh(filename) {
+    //Your code here
+    myMesh = new TriMesh();
+    myPromise = asyncGetFile(filename);
+    // We define what to do when the premise is resolved with the then() call,
+    // and what to do when the promise is rejected with the catch() call.
+    myPromise.then((retrievedText) => {
+       myMesh.loadFromOBJ(retrievedText);
+       console.log("Yay! got the file");
+    })
+    .catch(
+       (reason) => {
+           console.log("handle rejected promise (" + reason + ") here.");
+       });
+}
+
 function startup() {
   // Get the context
   canvas = document.getElementById("canvas");
@@ -52,6 +245,13 @@ function startup() {
   if (!gl) {
     return;
   }
+
+//   setupShaders();
+
+  document.onkeydown = handleKeyDown;
+  document.onkeyup = handleKeyUp;
+
+  setupMesh("cow.obj");
 
   // setup GLSL programs and lookup locations
   envmapProgramInfo = webglUtils.createProgramInfo(
@@ -129,6 +329,8 @@ function startup() {
   FOV = degToRad(65);
   var cameraYRotationRadians = degToRad(0);
 
+  cameraPosition = [Math.cos(0 * .1) * 2, 0, Math.sin(0 * .1) * 2];
+
   var spinCamera = true;
   // Get the starting time.
   initial = 0;
@@ -163,8 +365,8 @@ function draw(time) {
         m4.perspective(FOV, aspect, 1, 2000);
 
     // camera going in circle 2 units from origin looking at origin
-    var cameraPosition = [Math.cos(time * .1) * 2, 0, Math.sin(time * .1) * 2];
-    var target = [0, 0, 0];
+    // var cameraPosition = [Math.cos(time * .1) * 2, 0, Math.sin(time * .1) * 2];
+    target = [0, 0, 0];
     var up = [0, 1, 0];
     // Compute the camera's matrix using look at.
     var cameraMatrix = m4.lookAt(cameraPosition, target, up);
@@ -186,17 +388,50 @@ function draw(time) {
     var viewDirectionProjectionInverseMatrix =
         m4.inverse(viewDirectionProjectionMatrix);
 
-    // Draw the obj, whatever it is
-    gl.useProgram(envmapProgramInfo.program);
-    webglUtils.setBuffersAndAttributes(gl, envmapProgramInfo, cubeBufferInfo);
-    webglUtils.setUniforms(envmapProgramInfo, {
-      u_world: worldMatrix,
-      u_view: viewMatrix,
-      u_projection: projectionMatrix,
-      u_texture: texture,
-      u_worldCameraPosition: cameraPosition,
-    });
-    webglUtils.drawBufferInfo(gl, cubeBufferInfo);
+
+    //Draw Mesh - first start with using phong shading for simplicity.
+    //ADD an if statement to prevent early drawing of myMesh
+    if (myMesh.loaded() == "meme") {
+        mvPushMatrix();
+        mat4.rotateY(mvMatrix, mvMatrix, degToRad(eulerY));
+        mat4.multiply(mvMatrix,vMatrix,mvMatrix);
+        setMatrixUniforms();
+        setLightUniforms(lightPosition,lAmbient,lDiffuse,lSpecular);
+    
+        if ((document.getElementById("polygon").checked) || (document.getElementById("wirepoly").checked))
+        {
+            setMaterialUniforms(shininess,kAmbient,
+                                kTerrainDiffuse,kSpecular); 
+            myMesh.drawTriangles();
+        }
+    
+        if(document.getElementById("wirepoly").checked)
+        {   
+            setMaterialUniforms(shininess,kAmbient,
+                                kEdgeBlack,kSpecular);
+            myMesh.drawEdges();
+        }   
+
+        if(document.getElementById("wireframe").checked)
+        {
+            setMaterialUniforms(shininess,kAmbient,
+                                kEdgeWhite,kSpecular);
+            myMesh.drawEdges();
+        }   
+        mvPopMatrix();
+    }
+
+    // Draw the cube
+    // gl.useProgram(envmapProgramInfo.program);
+    // webglUtils.setBuffersAndAttributes(gl, envmapProgramInfo, cubeBufferInfo);
+    // webglUtils.setUniforms(envmapProgramInfo, {
+    //   u_world: worldMatrix,
+    //   u_view: viewMatrix,
+    //   u_projection: projectionMatrix,
+    //   u_texture: texture,
+    //   u_worldCameraPosition: cameraPosition,
+    // });
+    // webglUtils.drawBufferInfo(gl, cubeBufferInfo);
 
     // Draw the skybox specifically
     gl.useProgram(skyboxProgramInfo.program);
@@ -208,4 +443,116 @@ function draw(time) {
     webglUtils.drawBufferInfo(gl, quadBufferInfo);
 
     requestAnimationFrame(draw);
+}
+
+//----------------------------------------------------------------------------------
+/**
+ * Setup the fragment and vertex shaders **for the phong shading model specifically!**
+ */
+function setupShaders() {
+    vertexShader = loadShaderFromDOM("shader-vs");
+    fragmentShader = loadShaderFromDOM("shader-fs");
+    
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+  
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+      alert("Failed to setup shaders");
+    }
+  
+    gl.useProgram(shaderProgram);
+  
+    shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+  
+    shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
+    gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
+  
+    shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+    shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+    shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
+    shaderProgram.uniformLightPositionLoc = gl.getUniformLocation(shaderProgram, "uLightPosition");    
+    shaderProgram.uniformAmbientLightColorLoc = gl.getUniformLocation(shaderProgram, "uAmbientLightColor");  
+    shaderProgram.uniformDiffuseLightColorLoc = gl.getUniformLocation(shaderProgram, "uDiffuseLightColor");
+    shaderProgram.uniformSpecularLightColorLoc = gl.getUniformLocation(shaderProgram, "uSpecularLightColor");
+    shaderProgram.uniformShininessLoc = gl.getUniformLocation(shaderProgram, "uShininess");    
+    shaderProgram.uniformAmbientMaterialColorLoc = gl.getUniformLocation(shaderProgram, "uKAmbient");  
+    shaderProgram.uniformDiffuseMaterialColorLoc = gl.getUniformLocation(shaderProgram, "uKDiffuse");
+    shaderProgram.uniformSpecularMaterialColorLoc = gl.getUniformLocation(shaderProgram, "uKSpecular");
+  }
+
+  //----------------------------------------------------------------------------------
+/**
+ * Loads Shaders
+ * @param {string} id ID string for shader to load. Either vertex shader/fragment shader
+ */
+function loadShaderFromDOM(id) {
+    var shaderScript = document.getElementById(id);
+    
+    // If we don't find an element with the specified id
+    // we do an early exit 
+    if (!shaderScript) {
+      return null;
+    }
+    
+    // Loop through the children for the found DOM element and
+    // build up the shader source code as a string
+    var shaderSource = "";
+    var currentChild = shaderScript.firstChild;
+    while (currentChild) {
+      if (currentChild.nodeType == 3) { // 3 corresponds to TEXT_NODE
+        shaderSource += currentChild.textContent;
+      }
+      currentChild = currentChild.nextSibling;
+    }
+   
+    var shader;
+    if (shaderScript.type == "x-shader/x-fragment") {
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+      shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+      return null;
+    }
+   
+    gl.shaderSource(shader, shaderSource);
+    gl.compileShader(shader);
+   
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert(gl.getShaderInfoLog(shader));
+      return null;
+    } 
+    return shader;
+  }
+
+//-------------------------------------------------------------------------
+/**
+ * Sends light information to the shader
+ * @param {Float32Array} loc Location of light source
+ * @param {Float32Array} a Ambient light strength
+ * @param {Float32Array} d Diffuse light strength
+ * @param {Float32Array} s Specular light strength
+ */
+function setLightUniforms(loc,a,d,s) {
+    gl.uniform3fv(shaderProgram.uniformLightPositionLoc, loc);
+    gl.uniform3fv(shaderProgram.uniformAmbientLightColorLoc, a);
+    gl.uniform3fv(shaderProgram.uniformDiffuseLightColorLoc, d);
+    gl.uniform3fv(shaderProgram.uniformSpecularLightColorLoc, s);
+}
+
+//-------------------------------------------------------------------------
+/**
+ * Sends material information to the shader
+ * @param {Float32} alpha shininess coefficient
+ * @param {Float32Array} a Ambient material color
+ * @param {Float32Array} d Diffuse material color
+ * @param {Float32Array} s Specular material color
+ */
+function setMaterialUniforms(alpha,a,d,s) {
+    gl.uniform1f(shaderProgram.uniformShininessLoc, alpha);
+    gl.uniform3fv(shaderProgram.uniformAmbientMaterialColorLoc, a);
+    gl.uniform3fv(shaderProgram.uniformDiffuseMaterialColorLoc, d);
+    gl.uniform3fv(shaderProgram.uniformSpecularMaterialColorLoc, s);
 }
